c4fb7610ba3c5c56d9221aadbd1a5a4c9a919748
frostydj <64118807+frostydj@users.noreply.github.com>
2022-07-05 12:48:03 +0000

    host

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Host for UpTimeRobot
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+);
+
+//  -------------------------
+
 const dotenv = require('dotenv');
 dotenv.config();
 
